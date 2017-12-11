3cb37c4d7d201a1f4de78722892f617cdfba4e94
Laurent Courty <lrntct@gmail.com>
2017-12-11 11:39:04 -0600

    init overland area and depth

--- src/node.c
+++ src/node.c
@@ -247,6 +247,8 @@ void node_initState(int j)
     Node[j].oldLatFlow = 0.0;
     Node[j].newLatFlow = 0.0;
     Node[j].losses = 0.0;                                                      //(5.1.007)
+    Node[j].couplingArea = 0.0;                                                //coupling
+    Node[j].overlandDepth = 0.0;                                               //coupling
     Node[j].couplingInflow = 0.0;                                              //coupling
 
 
